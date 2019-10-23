e09426c4b043e892fb141e9549865dd4f3fc175a
Evgeniy Roschin <evgeny689@yandex.ru>
2019-10-23 22:36:44 +0400

    правки в дз ко второй лекции

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,13 +1,35 @@
-let menuLinks = document.getElementsByClassName('menu__link');
-let subMenu = document.getElementsByClassName('menu_sub');
+// let menuLinks = document.getElementsByClassName('menu__link');
+// let subMenu = document.getElementsByClassName('menu_sub');
+let mainMenuItems = document.querySelectorAll("ul.menu_main>li");
 
-function openSubmenu1() {
-    subMenu[0].className = "menu menu_sub menu_active";
+function openSubmenu() {
+    let subMenuElement = document.getElementsByClassName("menu menu_sub");
+
+    if (subMenuElement) {
+        if (subMenuElement[0].className == "menu menu_sub menu_active") {
+            subMenuElement[0].className = "menu menu_sub";
+            return false;
+        }
+
+        if (subMenuElement[0].className == "menu menu_sub") {
+            subMenuElement[0].className = "menu menu_sub menu_active";
+            return false;
+        }
+    }
 }
 
-function openSubmenu2() {
-    subMenu[1].className = "menu menu_sub menu_active";
-}
+mainMenuItems[1].onclick = openSubmenu;
+mainMenuItems[2].onclick = openSubmenu;
 
-menuLinks[1].onclick = openSubmenu1;
-menuLinks[5].onclick = openSubmenu2;
+// function openSubmenu1() {
+//     subMenu[0].className = "menu menu_sub menu_active";
+//     return false;
+// }
+
+// function openSubmenu2() {
+//     subMenu[1].className = "menu menu_sub menu_active";
+//     return false;
+// }
+
+// menuLinks[1].onclick = openSubmenu1;
+// menuLinks[5].onclick = openSubmenu2;
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -4,35 +4,37 @@ let arrowNext = document.getElementsByClassName('slider__arrow_next');
 let arrowPrev = document.getElementsByClassName('slider__arrow_prev');
 let activeImage = 0;
 
+function deactivateElements() {
+    imagesArr[activeImage].className = "slider__item";
+    dotsArr[activeImage].className = "slider__dot";
+}
+
+function activateElements() {
+    imagesArr[activeImage].className = "slider__item slider__item_active";
+    dotsArr[activeImage].className = "slider__dot slider__dot_active";
+}
+
 function nextImage() {
     if (activeImage < imagesArr.length - 1) {
-        imagesArr[activeImage].className = "slider__item";
-        dotsArr[activeImage].className = "slider__dot";
+        deactivateElements();
         activeImage = activeImage + 1;
-        imagesArr[activeImage].className = "slider__item slider__item_active";
-        dotsArr[activeImage].className = "slider__dot slider__dot_active";
+        activateElements();
     } else {
-        imagesArr[activeImage].className = "slider__item";
-        dotsArr[activeImage].className = "slider__dot";
+        deactivateElements();
         activeImage = 0;
-        imagesArr[activeImage].className = "slider__item slider__item_active";
-        dotsArr[activeImage].className = "slider__dot slider__dot_active";
+        activateElements();
     }
 }
 
 function prevImage() {
     if (activeImage > 0) {
-        imagesArr[activeImage].className = "slider__item";
-        dotsArr[activeImage].className = "slider__dot";
+        deactivateElements();
         activeImage = activeImage - 1;
-        imagesArr[activeImage].className = "slider__item slider__item_active";
-        dotsArr[activeImage].className = "slider__dot slider__dot_active";
+        activateElements();
     } else {
-        imagesArr[activeImage].className = "slider__item";
-        dotsArr[activeImage].className = "slider__dot";
+        deactivateElements();
         activeImage = imagesArr.length - 1;
-        imagesArr[activeImage].className = "slider__item slider__item_active";
-        dotsArr[activeImage].className = "slider__dot slider__dot_active";
+        activateElements();
     }
 }
 
@@ -42,51 +44,41 @@ arrowPrev[0].onclick = prevImage;
 //понимаю что код ниже нужно оптимизировать, но не пойму как
 
 function dotAction0 () {
-    imagesArr[activeImage].className = "slider__item";
-    dotsArr[activeImage].className = "slider__dot";
+    deactivateElements();
     activeImage = 0;
-    imagesArr[activeImage].className = "slider__item slider__item_active";
-    dotsArr[activeImage].className = "slider__dot slider__dot_active";
+    activateElements();
 }
 
 dotsArr[0].onclick = dotAction0;
 
 function dotAction1 () {
-    imagesArr[activeImage].className = "slider__item";
-    dotsArr[activeImage].className = "slider__dot";
+    deactivateElements();
     activeImage = 1;
-    imagesArr[activeImage].className = "slider__item slider__item_active";
-    dotsArr[activeImage].className = "slider__dot slider__dot_active";
+    activateElements();
 }
 
 dotsArr[1].onclick = dotAction1;
 
 function dotAction2 () {
-    imagesArr[activeImage].className = "slider__item";
-    dotsArr[activeImage].className = "slider__dot";
+    deactivateElements();
     activeImage = 2;
-    imagesArr[activeImage].className = "slider__item slider__item_active";
-    dotsArr[activeImage].className = "slider__dot slider__dot_active";
+    activateElements();
 }
 
 dotsArr[2].onclick = dotAction2;
 
 function dotAction3 () {
-    imagesArr[activeImage].className = "slider__item";
-    dotsArr[activeImage].className = "slider__dot";
+    deactivateElements();
     activeImage = 3;
-    imagesArr[activeImage].className = "slider__item slider__item_active";
-    dotsArr[activeImage].className = "slider__dot slider__dot_active";
+    activateElements();
 }
 
 dotsArr[3].onclick = dotAction3;
 
 function dotAction4 () {
-    imagesArr[activeImage].className = "slider__item";
-    dotsArr[activeImage].className = "slider__dot";
+    deactivateElements();
     activeImage = 4;
-    imagesArr[activeImage].className = "slider__item slider__item_active";
-    dotsArr[activeImage].className = "slider__dot slider__dot_active";
+    activateElements();
 }
 
 dotsArr[4].onclick = dotAction4;
